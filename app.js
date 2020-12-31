d5473201a352e9d82dac59cd9f1272e31f1ae284
fidelhen <fidelhen@gmail.com>
2020-12-30 20:05:41 -0500

    Complete ejs components and add adding todo action

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 let items = ["Buy food", "Cook food", "Eat food"]; 
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+// Routes
 app.get("/", function(req, res){
-    const today = new Date();
-    const options = {weekday: "long", day: "numeric", "month": "long"}
-
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: date.getDate(), newListItems: items});
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req,res){
+    res.render("about");
+});
+
+// Posts
 app.post("/", function(req, res){
     let item = req.body.newItem;
 
-    items.push(item);
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+    
 });
 
+// Server listner
 app.listen(3000, function(){
     console.log("Server is running on port 3000"); 
 });
