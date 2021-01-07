8dbfceffa961210ca7d6d180b467ebe799737e65
fidelhen <fidelhen@gmail.com>
2021-01-07 09:41:00 -0500

    Get rid of old keys file

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const keys = require("./keys");
 
 const app = express();
 
